1a17899097d72b53f0b2d738ab4f14643d1b9ca4
dKATO0424 <kato.daiki@outlook.com>
2026-01-05 22:59:14 +0900

    Reactのバージョンアップ対応

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/rabbithole/',
+  base: process.env.NODE_ENV === 'production' ? '/rabbithole/' : '/',
   plugins: [react()],
 })
